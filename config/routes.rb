8faa42a6dbc73fadb4a409fb69bf534815714004
Thiago-Cardoso <trovador182@gmail.com>
2019-02-28 18:39:16 -0300

    pagination

--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,10 @@ Rails.application.routes.draw do
     get 'welcome/index'
   end
   namespace :admins_backoffice do
-    get 'welcome/index'
+    get 'welcome/index' # Dashboard
+    resources :admins # Administradores
   end
+  
   devise_for :admins
   devise_for :users
  
